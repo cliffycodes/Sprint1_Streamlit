4a5ed9d08faa702715c94b216be27eb3a20e0d4c
cliffycodes <cliffyycodes@gmail.com>
2025-09-03 19:36:38 +0800

    updated requirements

--- app.py
+++ app.py
@@ -8,3 +8,14 @@ st.set_page_config(page_title="My first streamlit project",layout="wide")
 # Sidebar
 st.sidebar.title("Navigation")
 menu = st.sidebar.radio("What page should i open",["Test 1","Test 2"])
+
+data_dir = "data"
+image_dir = "images"
+plot_dir = "plots"
+
+if menu =="Overall" : 
+    st.title("Overall Summary")
+    st.write("This is an overview of the project")
+
+    overall_image_path = os.path.join(image_dir,"image.png")
+    st.image(overall_image_path,caption="Mochi",use_column_width=True)
