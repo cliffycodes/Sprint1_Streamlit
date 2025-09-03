01af1e83adf015020f5c4db2f52785ef8db07d92
cliffycodes <cliffyycodes@gmail.com>
2025-09-03 19:27:23 +0800

    Added app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title="My first streamlit project",layout="wide")
+
+# Sidebar
+st.sidebar.title("Navigation")
+menu = st.sidebar.radio("What page should i open",["Test 1","Test 2"])
