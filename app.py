4f67f9e083d3b756503985f0c37fc87ae390df38
cliffycodes <cliffyycodes@gmail.com>
2025-09-04 20:42:42 +0800

    Updated sidebar

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ import os
 st.set_page_config(page_title="My first streamlit project",layout="wide")
 
 # Sidebar
-st.sidebar.title("Navigation")
-menu = st.sidebar.radio("What page should i open",["Overview","Segment 1","Segment 2","Segment 3"])
+st.sidebar.title("Sprint 1 Project")
+menu = st.sidebar.radio("",["Overview","Segment 1","Segment 2","Segment 3"])
 
 data_dir = "data"
 image_dir = "images"
 plot_dir = "plots"
 
-if menu =="Test 1" : 
+if menu =="Overview" : 
     st.markdown("# Title")
     st.markdown("## Introduction")
     overall_image_path = os.path.join(image_dir,"test_screenshot.png")
